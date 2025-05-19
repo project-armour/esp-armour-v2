ca8f374db7d3968771ef538e722261f3903d6de0
***** *** <rohroexperiment@gmail.com>
2025-05-19 18:49:06 +0530

    add installer script

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+mpremote cp button.py :button.py
+mpremote cp command_handler.py :command_handler.py
+mpremote cp connection_handler.py :connection_handler.py
+mpremote cp display.py :display.py
+mpremote cp hardware.py :hardware.py
+mpremote cp heart_rate.py :heart_rate.py
+mpremote cp main.py :main.py
+mpremote cp pin_device.py :pin_device.py
+mpremote cp state.py :state.py
+mpremote cp utils.py :utils.py
+mpremote reset
