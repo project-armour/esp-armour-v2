4a69ddfba556197675de4ac0ef2d35df8a3d15fa
***** *** <rohroexperiment@gmail.com>
2025-10-21 12:51:25 +0530

    main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ netman = NetworkManager()
 bt = BluetoothHandler(config['name'], cmd)
 
 state.set('status', "Off")
+state.set('bt_state', "logo")
 state.set('bpm', 0)
 state.set('button', "")
 state.set('network', 'Disconnected')
@@ -21,15 +22,20 @@ display.set_line(2, 'Net: {network}')
 
 def bluetooth_ready():
     state.set('status', "Ready")
+    display.set_bitmap("ready")
+    state.set("bt_state", "ready")
 
 def bluetooth_connect(device):
     state.set('status', "Connected")
     display.set_bitmap("connected")
-    print("connected")
+    state.set("bt_state", "connected")
+
 
 def bluetooth_disconnect(device):
     state.set('status', "Disconnected")
-    display.set_bitmap("logo")
+    display.set_bitmap("ready")
+    state.set("bt_state", "ready")
+
 
 bt.on("ready", bluetooth_ready)
 bt.on("connect", bluetooth_connect)
@@ -48,10 +54,29 @@ def heart_rate_get(bpm):
 
 heart_rate_sensor.on("heart_rate", heart_rate_get)
 
+async def panic():
+    display.set_bitmap('panic')
+    buzzer.on()
+    await sleep_ms(2000)
+    display.set_bitmap(state.query("bt_state"))
+
+async def fake_call_cb():
+    display.set_bitmap('fake_call')
+    await sleep_ms(2000)
+    display.set_bitmap(state.query("bt_state"))
+
+async def flashbang():
+    flash.value(1 - flash.value())
+
+
 trigger.on('single', button_press, bt, 'short1')
+trigger.on('single', flashbang)
+
 trigger.on('long', button_press, bt, 'long1')
+trigger.on('long', panic)
 
 fake_call.on('single', button_press, bt, 'short2')
+fake_call.on('single', fake_call_cb)
 fake_call.on('long', button_press, bt, 'long2')
 
 def network_connected(ssid):
